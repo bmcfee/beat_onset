afa92bdd079a862e44ac828fb9d565230986c2e4
Brian McFee <brm2132@columbia.edu>
2014-01-30 10:48:40 -0500

    cleanup experiment script

--- code/run_experiment.sh
+++ code/run_experiment.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 FEATURES=/q/porkpie/porkpie-p0/drspeech/data/beat/features
-PREDICTIONS=~bmcfee/git/beat_onset/data/predictions
+PREDICTIONS=~bmcfee/git/beat_onset/data/predictions_final
 N_JOBS=16
 
-for DATA in rwcj jazz_roger mirex2006 smc_dataset2 
+for DATA in smc_dataset2 
 do
     for spec in sparse full harmonic percussive lowrank 
     do
-#         for log in '' -l -c
-        for log in -c
-        do
-            for quant in '' '-q 0.25' '-q 0.5' '-q 0.75'
-            do 
-                ./beat_predictor.py $log $quant -s $spec -j $N_JOBS \
-                    $FEATURES/$DATA/\*.pickle \
-                    $PREDICTIONS/$DATA/
-            done
+        for quant in '' '-m'
+        do 
+            ./beat_predictor.py $log $quant -s $spec -j $N_JOBS \
+                $FEATURES/$DATA/\*.pickle \
+                $PREDICTIONS/$DATA/
         done
     done
 done
